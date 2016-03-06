a128053524ae0ab5a5b5eedceab708b3ca3c7190
Evgeniia Bondini <esemindieieva@gmail.com>
2016-03-06 19:05:45 +0000

    update main.html, server.js

--- src/server.js
+++ src/server.js
@@ -2,12 +2,17 @@ var urlShotener = require("../src/url-shortener")
 var express = require('express');
 var app = express();
 
-   app.get('/:dateString', function (req, res) {
-        var urlInput = req.params.dateString;
-        console.log("Param: " + urlInput);
-        var response = urlShotener.shotener(urlInput);
-        console.log('Response: ' + response);
-        res.send(response);
-    })
+
+app.get('/shorten', function (req, res) {
+    //  var urlInput = req.params.dateString;
+     console.log("Param: " + req.params);
+     var response = urlShotener.shotener(urlInput);
+     console.log('Response: ' + response);
+     res.send(response);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 module.exports.app = app;
